25774f5c659a8e83e4b62f1e158ca6ec8fcad2cb
Svitlana Horodylova <horodylova.sv@gmail.com>
2024-11-28 13:53:29 +0000

    received the board

--- src/handlers/createIssueHandler.js
+++ src/handlers/createIssueHandler.js
@@ -1,6 +1,6 @@
 import api, { route } from '@forge/api';
 
-export async function fetchBoards() {
+export async function fetchBoards({ boardName }) {
     try {
         const response = await api.asApp().requestJira(route`/rest/agile/1.0/board`, {
             method: 'GET',
@@ -13,11 +13,16 @@ export async function fetchBoards() {
             throw new Error(`Error fetching boards: ${response.status} ${response.statusText}`);
         }
 
-        const data = await response.json();
-        console.log('Boards:', data);
-        return data;
+        const boards = await response.json();
+        const board = boards.values.find((b) => b.name === boardName);
+            
+        if(!board) {
+            throw new Error (`Board "{boardName}" not found.`)
+        }
+        return board;
     } catch (error) {
-        console.error('Failed to fetch boards:', error);
+        console.error(`Failed to fetch board: ${error.message}`);
         throw error;
     }
 }
+
